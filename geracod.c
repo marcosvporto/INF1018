bd3cb583f26d3015c31a603a9ad503f5726b9eac
marcosvporto <39508000+marcosvporto@users.noreply.github.com>
2018-06-27 17:54:23 -0300

    Add files via upload

--- geracod.c
+++ geracod.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-#define MAX_BYTES 1
+#define MAX_BYTES 2000
 #define MAX_TAM_LINHA 200
 #define MAX_LINHAS 20
 
@@ -30,7 +30,6 @@ static void error (const char *msg, int line){
 	exit(EXIT_FAILURE);
 }
 
-
 void preenche_cte(int cte , unsigned char *v){
 		int i;	
 		for(i=0;i<4;i++){
@@ -40,26 +39,197 @@ void preenche_cte(int cte , unsigned char *v){
 
 } 
 
-funcp geracod(FILE *myfp){
+void retorno_de_constante(unsigned char *codigo,int *byte_corrente, int constante){
 
-	unsigned char *codigo ={"0x55","0x48","0x89","0xe5","0x48","0x83","0xec","0x10","0xb8","0x00","0x00","0x00","0x00","0xc9","0xc3"};//(unsigned char *)malloc(MAX_BYTES);
-	/*unsigned char prologo[8] = { "0x55","0x48","0x89","0xe5","0x48","0x83","0xec","0x10" };
-	unsigned char epilogo[2] = { "0xc9","0xc3" };
-	unsigned char retv[2]    = {"0x8b","0x45"};
-	unsigned char retv1[1]   = {"0xfc"};
-	unsigned char retv2[1]   = {"0xf8"};
-	unsigned char retv3[1]   = {"0xf4"};
-	unsigned char retv4[1]   = {"0xf0"};
-	unsigned char retp[1]    = {"0x89"};
-	unsigned char retp1[]   = {"0xf8"};
-	unsigned char retp2[]   = {"0xf0"};
-	unsigned char retcte[]  = {"0xb8"};	
+	unsigned char v[4];
+	preenche_cte (constante,v);
+	memcpy(codigo+*byte_corrente,v,sizeof(v));
+	*byte_corrente += sizeof(v);
+	
+
+}
+void atribuicao(unsigned char *codigo,int *byte_corrente,char var0,int idx0,char var1, int idx1){
+/*
+ * var0 é o tipo da variavel que receberá o dado e pode ser v ou p
+ * idx0 é o numero da variavel que receberá o dado e pode ser 1, 2, 3 ou 4
+ * var1 é o tipo da variavel que contém o dado e pode ser v, p ou $
+ * idx1 é o numero da variavel que contém o dado e pode ser qualquer numero
+ */	unsigned char v[4];
+	unsigned char cte2v[2] = {0xc7,0x45};
+	if(var1 == '$'){// atribuição de constante
+		
+		preenche_cte(idx1,v);
+		
+		if(var0=='v'){// para uma variavel
+			//preenche_cod(codigo,byte_corrente,cte2v,sizeof(cte2v));
+			memcpy(codigo+*byte_corrente,cte2v,sizeof(cte2v));
+			
+			*byte_corrente+=sizeof(cte2v);
+			printf(" pos_cod = %d\n",*byte_corrente);
+ 			if(idx0==1){//para v1
+				codigo[*byte_corrente] = 0xfc;
+				(*byte_corrente)++;
+				printf(" pos_cod = %d\n",*byte_corrente);
+			}
+			else if(idx0==2){//para v2
+				codigo[*byte_corrente] = 0xf8;
+				(*byte_corrente)++;
+			}
+			else if(idx0==3){//para v3
+				codigo[*byte_corrente] = 0xf4;
+				(*byte_corrente)++;
+			}
+			else {//para v4
+				codigo[*byte_corrente] = 0xf0;
+				(*byte_corrente)++;
+			}
+		}
+		else if(idx0==1){//para p1
+			codigo[*byte_corrente] = 0xbf;
+			(*byte_corrente)++;
+		}else{//para p2
+			codigo[*byte_corrente] = 0xbe;
+			(*byte_corrente)++;
+		}
+		memcpy(codigo+*byte_corrente,v,sizeof(v));
+		*byte_corrente += sizeof(v);
+	}
+	else if(var1=='p'){//de um parametro
+		codigo[*byte_corrente] = 0x89;
+		(*byte_corrente)++;
+		if(var0=='v'){//para uma variavel
+			codigo[*byte_corrente] = idx1==1?  0x7d : 0x75;// de p1 ou de p2?
+			(*byte_corrente)++;
+			if(idx0==1){//para v1
+				codigo[*byte_corrente] = 0xfc;
+				(*byte_corrente)++;
+				printf(" pos_cod = %d\n",*byte_corrente);
+			}
+			else if(idx0==2){//para v2
+				codigo[*byte_corrente] = 0xf8;
+				(*byte_corrente)++;
+			}
+			else if(idx0==3){//para v3
+				codigo[*byte_corrente] = 0xf4;
+				(*byte_corrente)++;
+			}
+			else {//para v4
+				codigo[*byte_corrente] = 0xf0;
+				(*byte_corrente)++;
+			}
+		}
+		else{//para outro parametro
+			if(idx0 != idx1){
+				codigo[*byte_corrente] = idx1==1? 0xfe :  0xf7;// de p1 ou de p2?
+				(*byte_corrente)++;
+
+			}
+		}
+	}
+	else{//de uma variavel
+		codigo[*byte_corrente] = 0x8b; //quando quando é atribuição entre um parametro e uma variavel só vai variar o primeiro byte 8b ou 89
+		(*byte_corrente)++;//modificar o programa para para dimminuir o código caso dê tempo
+		if(var0=='p'){// para um parametro
+			codigo[*byte_corrente] = idx1==1? 0x7d :  0x75; // para p1 pu p2?
+			(*byte_corrente)++;
+			switch(idx1){// de qual variavel?
+				case 1:{
+					codigo[*byte_corrente] = 0xfc;
+					(*byte_corrente)++;
+					break;
+				}
+				case 2:{
+					codigo[*byte_corrente] = 0xf8;
+					(*byte_corrente)++;
+					break;
+				}
+				case 3:{
+					codigo[*byte_corrente] = 0xf4;
+					(*byte_corrente)++;
+					break;
+				}
+				case 4:{
+					codigo[*byte_corrente] = 0xf0;
+					(*byte_corrente)++;
+					break;
+				}
+			}
+		}
+		else{//para outra variavel
+			codigo[*byte_corrente] = 0x4d;	
+			(*byte_corrente)++;
+			switch(idx1){// de qual variavel?
+				case 1:{
+					codigo[*byte_corrente] = 0xfc;
+					(*byte_corrente)++;
+					break;
+				}
+				case 2:{
+					codigo[*byte_corrente] = 0xf8;
+					(*byte_corrente)++;
+					break;
+				}
+				case 3:{
+					codigo[*byte_corrente] = 0xf4;
+					(*byte_corrente)++;
+					break;
+				}
+				case 4:{
+					codigo[*byte_corrente] = 0xf0;
+					(*byte_corrente)++;
+					break;
+				}
+			}
+			codigo[*byte_corrente] = 0x89;
+			(*byte_corrente)++;
+			codigo[*byte_corrente] = 0x4d;	
+			(*byte_corrente)++;
+			if(idx0==1){//para v1
+				codigo[*byte_corrente] = 0xfc;
+				(*byte_corrente)++;
+				printf(" pos_cod = %d\n",*byte_corrente);
+			}
+			else if(idx0==2){//para v2
+				codigo[*byte_corrente] = 0xf8;
+				(*byte_corrente)++;
+			}
+			else if(idx0==3){//para v3
+				codigo[*byte_corrente] = 0xf4;
+				(*byte_corrente)++;
+			}
+			else {//para v4
+				codigo[*byte_corrente] = 0xf0;
+				(*byte_corrente)++;
+			}
+
+		}
+	}
+}
+
+funcp geracod(FILE *myfp){
+	funcp f;
+	unsigned char *codigo;
+	
+
+	
+	
+	unsigned char prologo[8] = {0x55,0x48,0x89,0xe5,0x48,0x83,0xec,0x10};
+	unsigned char epilogo[2] = {0xc9,0xc3 };
+	unsigned char retv[2]    = {0x8b,0x45};
+	unsigned char retv1[1]   = {0xfc};
+	unsigned char retv2[1]   = {0xf8};
+	unsigned char retv3[1]   = {0xf4};
+	unsigned char retv4[1]   = {0xf0};
+	unsigned char retp[1]    = {0x89};
+	unsigned char retp1[1]   = {0xf8};
+	unsigned char retp2[1]   = {0xf0};
+	unsigned char retcte[1]  = {0xb8};	
 	char *linha = (char*)malloc(MAX_TAM_LINHA);
 	int byte_corrente = 0;
 	int line = 1;
 	int c;
-	
-	/*if(codigo=NULL){
+	codigo = (unsigned char*)malloc(MAX_BYTES);
+	if(codigo==NULL){
 		fprintf(stderr, "Falta de memória ao alocar bloco de codigo\n");
 		exit(0);
 	}	
@@ -67,42 +237,94 @@ funcp geracod(FILE *myfp){
 	byte_corrente += sizeof(prologo);
 
 	
-	/*while ((c = fgetc(myfp)) != EOF ){
+	while ((c = fgetc(myfp)) != EOF ){
 		
 		switch(c){
 
 			
-			case 'r': {/* retorno /
+			case 'r': {/* retorno */
 				char var0;
 				int idx0;
-				int i;
+				
 				if (fscanf (myfp, "et %c%d", &var0, &idx0) != 2)
 					error("comando invalido",line);
-				if(var0=='$'){
+				if(var0=='$'){//retorno de constante
+					
 					codigo[byte_corrente] = retcte[0];
 					byte_corrente++;
-					unsigned char v[4];
-					preenche_cte (idx0,v);
-					memcpy(codigo+byte_corrente,v,sizeof(v));
-					byte_corrente += sizeof(v);
-					memcpy(codigo+byte_corrente,epilogo,sizeof(epilogo));
-					byte_corrente += sizeof(epilogo);
+					retorno_de_constante(codigo,&byte_corrente,idx0);
 					
+					
+				}
+				else if(var0=='v'){// retorno de variavel local
+					memcpy(codigo+byte_corrente,retv,sizeof(retv));
+					byte_corrente +=sizeof(retv);
+					if(idx0==1){
+						codigo[byte_corrente] = retv1[0];
+						byte_corrente++;
+					}
+					else if(idx0==2){
+						codigo[byte_corrente] = retv2[0];
+						byte_corrente++;
+					}
+					else if(idx0==3){
+						codigo[byte_corrente] = retv3[0];
+						byte_corrente++;
+					}
+					else{
+						codigo[byte_corrente] = retv4[0];
+						byte_corrente++;
+					}
+				}
+				else{//retorno de parametro
+					codigo[byte_corrente] = retp[0];
+					byte_corrente++;
+					if(idx0==1){
+						codigo[byte_corrente] = retp1[0];
+						byte_corrente++;
+					}
+					else{
+						codigo[byte_corrente] = retp2[0];
+						byte_corrente++;
+					}
 				}	
 				printf("%d ret %c%d\n", line,var0,idx0);
+				memcpy(codigo+byte_corrente,epilogo,sizeof(epilogo));
+				byte_corrente += sizeof(epilogo);
+					
 				break;
 			}
 			case 'v':
-			case 'p':{/* atribuicao e op. aritmetica /
+			case 'p':{/* atribuicao e op. aritmetica */
 				char var0 = c, var1, op;
 				int idx0, idx1;
 				if(fscanf(myfp, "%d %c= %c%d", &idx0, &op, &var1, &idx1 ) != 4)
 					error("comando invalido", line);
+				switch(op){
+					case ':':{printf("chegou\n");
+						atribuicao(codigo,&byte_corrente,var0,idx0,var1,idx1);
+						break;
+					}
+					case '*':{
+						//multiplicacao(codigo,&byte_corrente,var0,idx0,var1,idx1);
+						break;
+					}
+					case '+':{
+						//soma(codigo,&byte_corrente,var0,idx0,var1,idx1);
+						break;
+					}
+					case '-':{
+						//subtracao(codigo,&byte_corrente,var0,idx0,var1,idx1);
+						break;
+					}
+
+				}
 				printf("%d %c%d %c= %c%d\n", line, var0, idx0, op, var1, idx1);
+				
 				break;
 
 			} 
-			case 'i':{/* desvio condicional /
+			case 'i':{/* desvio condicional */
 				char var0;
 				int idx0, n1, n2;
 				if(fscanf(myfp, "f %c%d %d %d", &var0, &idx0, &n1, &n2) != 4)
@@ -111,7 +333,7 @@ funcp geracod(FILE *myfp){
 				break;
 
 			}
-			case 'g':{/* desvio incondicional /
+			case 'g':{/* desvio incondicional */
 				int n1;
 				if(fscanf(myfp, "o %d", &n1) != 1)
 					error("comando invalido", line);
@@ -121,11 +343,14 @@ funcp geracod(FILE *myfp){
 			default: error("comando desconhecido", line);
 
 		}
+		
 		line++;
 		fscanf(myfp, " ");
-	}*/
-	
-	return (funcp) codigo;
+	}
+	printf("dump de codigo\n");
+	dump(codigo,byte_corrente);
+	f = (funcp)codigo;
+	return f;
 
 }
 
@@ -133,7 +358,7 @@ int main(/* int argc, char **argv*/){
 
 
 	FILE *myfp;
-	int res;
+	int res=0;
 	funcp sb; 
 	if((myfp = fopen("programa", "r")) == NULL){
 		perror("nao conseguiu abrir arquivo");
@@ -141,8 +366,8 @@ int main(/* int argc, char **argv*/){
 
 	}
 	sb = geracod(myfp);
-	res = (*sb)();
-	printf(" retorno da funcao sb%d\n",res);
+	res = (*sb)(12,123);
+	printf(" retorno da funcao sb = %d\n",res);
 	return 0;
 }
 
